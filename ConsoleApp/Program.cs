dc6a14a246d5be9ff660713fa61089fbe6ef961b
Paweł Grabacki <paw.grabacki@gmail.com>
2025-01-25 00:37:05 +0100

    datatypes & comments

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,12 +1,32 @@
-﻿namespace ConsoleApp
+﻿using System.Numerics;
+
+namespace ConsoleApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
 
+            //this is single line coment
 
-            Console.WriteLine("Hello, World! :)");
+            /*
+             this is multiline comment
+             */
+            
+            
+            //data types
+
+            int x = 10;             //whole number
+            double y = 2.32;        //foating point number
+            bool z = true;          //stores value either true or false
+            char c = 'a';           //single character
+            string s = "test";      //multiple characters stored in array 
+        
+
+            
+
+
+            Console.WriteLine(s);
         }
     }
 }
