e7e8d475b68c61d913fded28d84b479b284cf1c4
Paweł Grabacki <paw.grabacki@gmail.com>
2025-02-03 22:35:25 +0100

    OOP Inheritace test

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -5,7 +5,17 @@ namespace ConsoleApp
 {
     internal class Program
     {
+
+
+
+
+
+
+
+
         static void Main(string[] args)
+
+
         {
 
             //this is single line coment
@@ -13,8 +23,8 @@ namespace ConsoleApp
             /*
              this is multiline comment
              */
-            
-            
+
+
             //data types
 
             int x = 1;             //whole number
@@ -29,17 +39,17 @@ namespace ConsoleApp
             //type casting----------------------------------------
 
             int g = (int)y; //manual casting  explicit
-          //  int h = c;      //automatic casting i need to check it out:)
+                            //  int h = c;      //automatic casting i need to check it out:)
 
 
             //converting using functions
             int k = Convert.ToInt32(c);
 
-                Console.WriteLine(k);
-          
+            Console.WriteLine(k);
 
 
-            Console.WriteLine("This value chanhed its type from double to int (manual) "+g);
+
+            Console.WriteLine("This value chanhed its type from double to int (manual) " + g);
             //Console.WriteLine("This value chanhed its type from double to int(automatic) "+h);
             Console.WriteLine("This value chanhed its type from double to int (By using built in methods) " + k);
 
@@ -49,9 +59,9 @@ namespace ConsoleApp
 
             Console.WriteLine("Loops\n" + divider);//also includes concatenation
 
-            for (int i = 1; i<=10; i++)  //for loop performs opertions specified number of times
+            for (int i = 1; i <= 10; i++)  //for loop performs opertions specified number of times
             {
-                Console.WriteLine("For loop with displayed iterator "+i);
+                Console.WriteLine("For loop with displayed iterator " + i);
 
             }
             int n = 0;
@@ -63,8 +73,8 @@ namespace ConsoleApp
             do
             {
                 Console.WriteLine("Do while executes at least once");
-                
-            }while (n <= 10);
+
+            } while (n <= 10);
 
 
             foreach (int item in a)// loop designed to operate on arrays and its items
@@ -75,14 +85,14 @@ namespace ConsoleApp
 
 
 
-            switch(x) //switch case statement allows to execute choosen case/code
+            switch (x) //switch case statement allows to execute choosen case/code
             {
                 case 1:
-                    Console.WriteLine("Case "+x);
+                    Console.WriteLine("Case " + x);
                     break;
 
                 case 10:
-                    Console.WriteLine("Case "+x);
+                    Console.WriteLine("Case " + x);
                     break;
 
 
@@ -90,13 +100,98 @@ namespace ConsoleApp
 
 
 
-            if (x == 1  || x==5) //allows to check if condition or conditions are true and based on that executes code
+            if (x == 1 || x == 5) //allows to check if condition or conditions are true and based on that executes code
             {
-                Console.WriteLine("Hello from if statement \n"+x);
+                Console.WriteLine("Hello from if statement \n" + x);
             }
-            else 
+            else
             {
-                Console.WriteLine("Hello from if statement "+x);
+                Console.WriteLine("Hello from if statement " + x);
+            }
+
+
+            //instanciating object and accesing their methods
+
+            Console.WriteLine("OOP \n" + divider);
+
+            Samolot samolot = new Samolot(10, 20, 300, "Testowy Samolot");
+            samolot.DisplayProperties();
+
+            samolot.SetName("F16");
+            samolot.SetVmax(1500);
+            samolot.DisplayProperties();
+
+            Odrzutowiec odrzutowiec = new Odrzutowiec(15, 25, 2500, "F22");
+            odrzutowiec.DisplayProperties();
+            odrzutowiec.IncreaseSpeed();
+
+            Turbosmigłowy turbo = new Turbosmigłowy(12, 18, 500, "Boeing");
+            turbo.DisplayProperties();
+            turbo.DestroyItself();
+
+
+
+        }
+        //OOP inherinence tests
+
+        public class Samolot
+        {
+            private int dlugosc;
+            private int szerokosc;
+            private int vmax;
+            private string name;
+
+            // Cnstructor
+            public Samolot(int i, int o, int p, string name)
+            {
+                this.dlugosc = i;
+                this.szerokosc = o;
+                this.vmax = p;
+                this.name = name;
+            }
+
+            // Metody
+            public void SetName(string u)
+            {
+                this.name = u;
+            }
+
+            public void SetVmax(int k)
+            {
+                this.vmax = k;
+            }
+
+            public void DisplayProperties()
+            {
+                Console.WriteLine("Maksymalna prędkość: " + vmax);
+                Console.WriteLine("Szerokość samolotu: " + szerokosc);
+                Console.WriteLine("Długość samolotu: " + dlugosc);
+                Console.WriteLine("Nazwa: " + name);
+            }
+        }
+
+        public class Odrzutowiec : Samolot
+        {
+            // Konstruktor klasy dziedziczącej musi wywołać konstruktor klasy bazowej
+            public Odrzutowiec(int i, int o, int p, string name) : base(i, o, p, name)
+            {
+            }
+
+            public void IncreaseSpeed()
+            {
+                Console.WriteLine("Increased speed");
+            }
+        }
+
+        public class Turbosmigłowy : Samolot
+        {
+            public Turbosmigłowy(int i, int o, int p, string name) : base(i, o, p, name)
+            {
+            }
+
+            public void DestroyItself()
+            {
+                Console.WriteLine("Destroyed itself");
             }
         }
     }
